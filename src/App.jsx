f7091c348dd31114edda6ed919a40cb822eee723
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2024-01-26 02:00:30 +0600

    About section has done with respnsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import styles from './App.module.css'
+import About from './Components/About/About'
 import Hero from './Components/Hero/Hero'
 import Navbar from './Components/Navbar/Navbar'
 
@@ -7,6 +8,7 @@ function App() {
     <div className={styles.App}>
       <Navbar></Navbar>
       <Hero></Hero>
+      <About></About>
     </div>
   )
 }
