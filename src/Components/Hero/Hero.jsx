080a48c48e70c0aa01f21b4875b32bcf1daf35c0
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2024-01-26 01:12:49 +0600

    Hero section responsiveness complete

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
     return (
         <section className={styles.container}>
             <div className={styles.content}>
-                <h1 className={styles.title}>Hi, I am Parvez Mahamud AA</h1>
+                <h1 className={styles.title}>Hi, I am Parvez</h1>
                 <p className={styles.description}>I’m a full-stack developer with 5 years
 of experience using React and NodeJS.
 Reach out if you’d like to learn more!</p>
