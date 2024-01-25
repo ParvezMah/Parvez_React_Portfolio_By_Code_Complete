9faacd018ec190960e8ba881f6196df66008dce6
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2024-01-26 03:54:37 +0600

    Experience section has done with responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import styles from './App.module.css'
 import About from './Components/About/About'
+import Experience from './Components/Experience/Experience'
 import Hero from './Components/Hero/Hero'
 import Navbar from './Components/Navbar/Navbar'
 
@@ -9,6 +10,7 @@ function App() {
       <Navbar></Navbar>
       <Hero></Hero>
       <About></About>
+      <Experience></Experience>
     </div>
   )
 }
--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -1,8 +1,49 @@
+import styles from "../Experience/Experience.module.css"
+import skills from "../../Components/Data/skills.json"
+import history from "../../Components/Data/history.json"
+
 const Experience = () => {
+    console.log('skills is : ', skills);
+    console.log('history is : ', history);
+
     return (
-        <div>
-            
-        </div>
+        <section className={styles.container} id="experience">
+            <h2 className={styles.title}>Experience</h2>
+            <div className={styles.content}>
+                <div className={styles.skills}>
+                    {
+                        skills.map((skill, id) => {
+                            return <div key={id} className={styles.skill}>
+                                <div className={styles.skillImgContainer}>
+                                    <img style={{ width: '75px' }}  src={skill.imageSrc} alt={skill.title} />
+                                </div>
+                                <p>{skill.title}</p>
+                            </div>
+                        })
+                    }
+                </div>
+                <ul className={styles.history}>
+                    {
+                        history.map((historyItem, id) => {
+                            return <li key={id} className={styles.historyItem}>
+                                <img 
+                                    src={historyItem.imageSrc} 
+                                    alt={historyItem.organisation}
+                                    style={{ width: '75px' }}
+                                />
+                                <div className={styles.historyItemDetails}>
+                                    <h3>{`${historyItem.role}, ${historyItem.organisation}`}</h3>
+                                    <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
+                                    <ul>{historyItem.experiences.map((experience, id) => {
+                                        return <li key={id}>{experience}</li>
+                                    })}</ul>
+                                </div>
+                            </li>
+                        })
+                    }
+                </ul>
+            </div>
+        </section>
     );
 };
 
